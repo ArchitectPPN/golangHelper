f6b2e5b89274a01ce960c7a02bd81c4b15f124f0
architectPPN <ng_sou@163.com>
2023-04-08 16:14:24 +0800

    初始化 map

---
+++ mapHelper.go
@@ -0,0 +1,22 @@
+package golangHelper
+
+// checkKeyExist 检查key是否存在
+func checkKeyExist(targetMap map[int]string, targetKey int) bool {
+	// map为nil,返回false
+	if targetMap == nil {
+		return false
+	}
+
+	_, exist := targetMap[targetKey]
+	if !exist {
+		return false
+	}
+
+	//默认返回false
+	return true
+}
+
+// initMap 初始化map
+func initMap(targetMap map[int]string) map[int]string {
+	return make(map[int]string)
+}
